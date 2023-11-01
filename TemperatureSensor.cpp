d8cdbc9c560808158cf72529461402266ca95fa9
Zefek <32429493+Zefek@users.noreply.github.com>
2023-11-01 13:29:44 +0100

    Add files via upload

---
+++ TemperatureSensor.cpp
@@ -0,0 +1,141 @@
+#include "Arduino.h" 
+#include "TemperatureSensor.h"
+
+unsigned int TemperatureSensor::syncIndex1 = 0;
+unsigned int TemperatureSensor::syncIndex2 = 0;
+bool TemperatureSensor::received = false;
+int TemperatureSensor::interruptPin = 2;
+unsigned long TemperatureSensor::receivedMillis = 0;
+unsigned long TemperatureSensor::timings[RING_BUFFER_SIZE];
+void (*TemperatureSensor::TemperatureChanged)(double, int);
+
+bool TemperatureSensor::isSync(unsigned int idx)
+{
+  unsigned long t0 = timings[(idx+RING_BUFFER_SIZE-1) % RING_BUFFER_SIZE];
+  unsigned long t1 = timings[idx];
+  // on the temperature sensor, the sync signal
+  // is roughtly 9.0ms. Accounting for error
+  // it should be within 8.0ms and 10.0ms
+  if (t0>(SEP_LENGTH-100) && t0<(SEP_LENGTH+100) &&
+    t1>(SYNC_LENGTH-1000) && t1<(SYNC_LENGTH+1000) &&
+    digitalRead(interruptPin) == HIGH) {
+    return true;
+  }
+  return false;
+}
+
+TemperatureSensor::TemperatureSensor(int interruptPin, void (*temperatureChanged)(double, int))
+{
+  interruptPin = interruptPin;
+  received = false;
+  syncIndex1 = 0;
+  syncIndex2 = 0;
+  receivedMillis = 0;
+  TemperatureChanged = temperatureChanged;
+}
+
+void TemperatureSensor::Init()
+{
+  digitalWrite(11, HIGH);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handler, CHANGE);
+}
+
+void TemperatureSensor::handler()
+{
+  static unsigned long duration = 0;
+  static unsigned long lastTime = 0;
+  static unsigned int ringIndex = 0;
+  static unsigned int syncCount = 0;
+  if (received == true || millis() - receivedMillis <= 1000) {
+    return;
+  }
+  long time = micros();
+  duration = time - lastTime;
+  lastTime = time;
+
+  // store data in ring buffer
+  ringIndex = (ringIndex + 1) % RING_BUFFER_SIZE;
+  timings[ringIndex] = duration;
+
+  // detect sync signal
+  if (isSync(ringIndex)) {
+    syncCount++;
+    // first time sync is seen, record buffer index
+    if (syncCount == 1) {
+      syncIndex1 = (ringIndex+1) % RING_BUFFER_SIZE;
+    } 
+    else if (syncCount == 2) {
+      // second time sync is seen, start bit conversion
+      syncCount = 0;
+      syncIndex2 = (ringIndex+1) % RING_BUFFER_SIZE;
+      unsigned int changeCount = (syncIndex2 < syncIndex1) ? (syncIndex2+RING_BUFFER_SIZE - syncIndex1) : (syncIndex2 - syncIndex1);
+      // changeCount must be 86 -- 32 bits x 2 + 2 for sync
+      if (changeCount != 92) {
+        received = false;
+        syncIndex1 = 0;
+        syncIndex2 = 0;
+      } 
+      else {
+        received = true;
+      }
+    }
+  }
+}
+
+bool TemperatureSensor::Read(int from, int to, unsigned long* value)
+{
+  unsigned long temp = 0;
+  bool negative = false;
+  bool fail = false;
+  for(unsigned int i=(syncIndex1+from)%RING_BUFFER_SIZE; i!=(syncIndex1+to)%RING_BUFFER_SIZE; i=(i+2)%RING_BUFFER_SIZE) {
+    unsigned long t0 = TemperatureSensor::timings[i], t1 = TemperatureSensor::timings[(i+1)%RING_BUFFER_SIZE];
+    if (t0>(SEP_LENGTH-100) && t0<(SEP_LENGTH+100)) {
+      if (t1>(BIT1_LENGTH-1000) && t1<(BIT1_LENGTH+1000)) {
+        temp = (temp << 1) + 1;
+      } 
+      else if (t1>(BIT0_LENGTH-1000) && t1<(BIT0_LENGTH+1000)) {
+        temp = (temp << 1) + 0;
+      } 
+      else {
+        fail = true;
+      }
+    } 
+    else {
+      fail = true;
+    }
+  }
+  if(!fail)
+  {
+    *value = temp;
+  }
+  return fail;
+}
+
+void TemperatureSensor::CheckTemperature()
+{
+  if (received == true) {
+    // disable interrupt to avoid new data corrupting the buffer
+    detachInterrupt(digitalPinToInterrupt(TemperatureSensor::interruptPin));
+    // loop over the lowest 12 bits of the middle 2 bytes
+    bool fail = false;
+    unsigned long temp = 0;
+    unsigned long channel = 1;
+    fail = TemperatureSensor::Read(32, 56, &temp);
+
+    if (!fail) 
+    {
+      //Serial.print(((((double)temp*0.1)-90)-32)*((double)5/9));
+      TemperatureSensor::Read(72, 80, &channel);
+      TemperatureChanged(((((double)temp*0.1)-90)-32)*((double)5/9), (int)channel);
+      receivedMillis = millis();
+    } 
+    else 
+    {
+      //Serial.println("Decoding error.");
+    }
+    received = false;
+    syncIndex1 = 0;
+    syncIndex2 = 0;
+    attachInterrupt(digitalPinToInterrupt(interruptPin), handler, CHANGE);
+  }
+}
--- rizeni_teploty_kotel.ino
+++ rizeni_teploty_kotel.ino
@@ -1,72 +1,113 @@
-#include <LiquidCrystal_I2C.h>
 #include <avr/wdt.h>
-#include <virtuabotixRTC.h>
+#include <Ds1302.h>
+#include "display.h"
+#include "TemperatureSensor.h"
+#include "TemperatureList.h"
 
 #define I2C_ADDR    0x27
 #define LCD_COLUMNS 16
 #define LCD_LINES   2
 #define BETA  3950
-#define POTPIN  0
-#define TEMPPIN  1
-#define OUTTEMPPIN  2
+#define TEMPPIN  A0
+#define OUTTEMPPIN  A1
 #define NORMTEMP 1 / (25 + 273.15)
 #define R0 9800
 #define R 10000
-#define EKVITERM_KOEF 2.34
-#define EKVITERM_SLOPE -0.7
+#define MOREHEATINGRELAYPIN 8
+#define LESSHEATINGRELAYPIN 9
+#define HEATINGPUMPRELAYPIN 10
 
-LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
-virtuabotixRTC myRtc(2, 3, 4);
+void OutsideTemperatureChanged(double temperature, int channel);
+
+Display lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+Ds1302 rtc(5, 3, 4);
+TemperatureSensor outsideTemperatureSensor(2, OutsideTemperatureChanged);
+TemperatureList tempList;
 unsigned long relayOnMillis = 0;
 unsigned long relayOffMillis = 0;
 unsigned long lastReadCelsius = 0;
 unsigned long lastReadOutTemperature = 0;
-unsigned long lastSetOutsideTemperature = 0;
+unsigned long currentMillis = 0;
+unsigned long averageCelsiusMillis = 0;
 long interval = 0;
 long position = 0;
 bool relayOn = false;
-double value = 0;
-double celsius = 0;
+double value = 25;
+double celsius = 25;
+double averageCelsius = 0;
+int averageCelsiusCount = 0;
 int direction = 0;
-bool heatingOff = false;
-int inputTemperature = 100;
-double outsideTemperature = 99;
-double tempOutsideTemperature = 0;
-int countOutsideTemperature = 0;
+bool heatingOff = true;
+double inputTemperature = 25;
+double outsideTemperature = 25;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Started.");
+  analogReference(EXTERNAL);
+  rtc.init();
+  lcd.Init(&rtc);
+  lcd.BackLight();
+  pinMode(MOREHEATINGRELAYPIN, OUTPUT);
+  pinMode(LESSHEATINGRELAYPIN, OUTPUT);
+  pinMode(HEATINGPUMPRELAYPIN, OUTPUT);
+  digitalWrite(MOREHEATINGRELAYPIN, HIGH);
+  digitalWrite(LESSHEATINGRELAYPIN, HIGH);
+  digitalWrite(HEATINGPUMPRELAYPIN, HIGH);
+  wdt_enable(WDTO_1S);
+  position = 60000;
+  outsideTemperatureSensor.Init();
+  lcd.SetCurrentHeatingTemperature(celsius);
+  lcd.SetInputTemperature((int)inputTemperature);
+  lcd.SetOutTemperature(outsideTemperature);
+  /*int hour, minute, second;
+  sscanf(__TIME__, "%d:%d:%d", &hour, &minute, &second);
+  Ds1302::DateTime dt;
+  dt.hour = hour;
+  dt.minute = minute;
+  dt.second = second;
+  dt.day = 28;
+  dt.month = 10;
+  dt.year = 2023;
+  rtc.setDateTime(&dt);*/
+}
+
+void OutsideTemperatureChanged(double temperature, int channel)
+{
+  lastReadOutTemperature = currentMillis;
+  if(channel == 1)
+  {
+    Ds1302::DateTime now;
+    rtc.getDateTime(&now);
+    tempList.Add(now.hour, now.minute, temperature);
+    outsideTemperature = tempList.GetAverageTemperature();
+    lcd.SetOutTemperature(outsideTemperature);
+  }
+}
 
 double getTemperature(double pinValue, int beta)
 {
   return 1 / (log(R0 / (1023 / pinValue - 1) / R) / beta + NORMTEMP) - 273.15;
 }
 
-void setup() {
-  analogReference(EXTERNAL);
-  lcd.init();
-  lcd.backlight();
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  //relayOffMillis = millis() - 60000;
-  wdt_enable(WDTO_1S);
-  position = 60000;
-  //myRtc.setDS1302Time(20, 21, 14, 1, 27, 2, 2023);
-}
-
-int getRequiredInsideTemperature()
-{
-  int hour = myRtc.hours;
-  if(hour >= 8 && hour <23)
-    return 22;
-  return 19;
-}
-
 void computeRequiredTemperature()
 {
-  value = (outsideTemperature * EKVITERM_SLOPE) + (getRequiredInsideTemperature() * EKVITERM_KOEF);
+  double newValue = (outsideTemperature * -0.005093696 * -0.005093696) + (outsideTemperature * -0.966171371) + 43.4724957;
+  Ds1302::DateTime now;
+  rtc.getDateTime(&now);
+  if(now.hour < 8 || now.hour >= 23)
+  {
+    newValue *= 0.87;
+  }
+  //value = (outsideTemperature * EKVITERM_SLOPE) + (getRequiredInsideTemperature() * EKVITERM_KOEF);
   //value =  EKVITERM_KOEF * getRequiredInsideTemperature() + (1 - EKVITERM_KOEF) * outsideTemperature;
-  lcd.setCursor(0, 0);
-  lcd.print(value);
+  if(newValue != value)
+  {
+    value = newValue;
+    //Serial.print("Required temperature : ");
+    //Serial.println(value);
+    lcd.SetRequiredTemperature(value);
+  }
 }
 
 void setRelay(long setTemperature, double currentTemperature)
@@ -77,110 +118,92 @@ void setRelay(long setTemperature, double currentTemperature)
   if(setTemperature - currentTemperature + 1 < 0 && position > 0)
   {
     direction = -1;
-    digitalWrite(8, HIGH);
+    digitalWrite(LESSHEATINGRELAYPIN, LOW);
     relayOn = true;
+    //Serial.print("Less heating relay on, interval: ");
+    //Serial.println(interval);
   }
   //value - celsis  - nastavená 50, naměřená 40 = 40-50=>10 => value > 0, direction = 1
   //value - celsius - nastavená 50, naměřená 50,68 = 50-50,68 = 0,68 - 0,5 = 0,18
   if(setTemperature - currentTemperature > 0 && position < 120000)
   {
     direction = 1;
-    digitalWrite(9, HIGH);
+    digitalWrite(MOREHEATINGRELAYPIN, LOW);
     relayOn = true;
+    //Serial.print("More heating relay on, interval: ");
+    //Serial.println(interval);
   }
 }
 
 void setRelayOff()
 {
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
+  digitalWrite(MOREHEATINGRELAYPIN, HIGH);
+  digitalWrite(LESSHEATINGRELAYPIN, HIGH);
   relayOn = false;
+  //Serial.println("Relay off");
 }
 
-void print2digits(int number) {
-  if (number >= 0 && number < 10) {
-    lcd.print("0");
-  }
-  lcd.print(number);
-}
-
-void printTime()
+void checkHeating()
 {
-  myRtc.updateTime();
-  lcd.setCursor(11, 0);
-  print2digits(myRtc.hours);
-  lcd.print(":");
-  print2digits(myRtc.minutes);
-}
-
-void printOutTemperature(double outTemperature)
-{
-  //celkem 6 míst -10.00
-  lcd.setCursor(10, 1);
-  if(outTemperature >= 0 && outTemperature < 10)
-  {
-    //8.98
-    lcd.print("  ");
-  }
-  if((outTemperature > -10 && outTemperature < 0) || outTemperature >= 10)
-  {
-    //-8.89
-    lcd.print(" ");
-  }
-  lcd.print(outTemperature);
-}
-
-void checkHeating(unsigned long currentMillis)
-{
-  if(!heatingOff && ((value >= 40 && inputTemperature < 39) || (value < 40 && inputTemperature < value - 2)))
+  if((!heatingOff && outsideTemperature >= 13) || inputTemperature < value - 2)
   {
     heatingOff = true;
+    digitalWrite(LESSHEATINGRELAYPIN, LOW);
+    digitalWrite(HEATINGPUMPRELAYPIN, HIGH);
     interval = 60000;
     direction = -1;
-    digitalWrite(8, HIGH);
     relayOn = true;
     relayOnMillis = currentMillis;
+    //Serial.println("Heating off");
   }
-  if(heatingOff && ((value >= 40 && inputTemperature > 40) || (value < 40 && inputTemperature > value + 2)))
+  if(heatingOff && outsideTemperature < 13  && inputTemperature >= value + 2)
   {
     heatingOff = false;
+    digitalWrite(HEATINGPUMPRELAYPIN, LOW);
+    //Serial.println("Heating on");
   }
-}
-
-void computeOutsideTemperature(unsigned long currentMillis)
-{
-  double currentTemperature = getTemperature(analogRead(3), 3435);
-  tempOutsideTemperature = (tempOutsideTemperature * countOutsideTemperature + currentTemperature) / (countOutsideTemperature + 1);
-  countOutsideTemperature++;
-
-  if(currentMillis - lastSetOutsideTemperature > 60000 || outsideTemperature >= 99)
-  {
-    outsideTemperature = tempOutsideTemperature;
-    printOutTemperature(outsideTemperature);
-    countOutsideTemperature = 1;
-    lastSetOutsideTemperature = currentMillis;    
-  }  
+  lcd.SetHeating(!heatingOff);
 }
 
 void computeInputTemperature()
 {
-  inputTemperature = getTemperature(analogRead(2), 3950);
-  lcd.setCursor(7, 0);
-  lcd.print(inputTemperature);
+  double newInputTemperature = getTemperature(analogRead(OUTTEMPPIN), 3950);
+  if(newInputTemperature < 0)
+  {
+    //Serial.println("Error read input temperature");
+    return;
+  }
+  if(newInputTemperature != inputTemperature)
+  {
+    inputTemperature = newInputTemperature;
+    lcd.SetInputTemperature((int)inputTemperature);
+  }
+  
 }
 
 void readCurrentHeatingTemperature()
 {
-  celsius = getTemperature(analogRead(TEMPPIN), 3950);
-  lcd.setCursor(0, 1);
-  lcd.print(celsius);
+  double newCelsius = getTemperature(analogRead(TEMPPIN), 3950);
+  if(newCelsius < 0)
+  {
+    //Serial.println("Error read current heating temperature");
+    return;
+  }
+  averageCelsius = (averageCelsius * averageCelsiusCount + newCelsius) / ++averageCelsiusCount;
+  if(averageCelsius != celsius && currentMillis - averageCelsiusMillis >= 1875)
+  {
+    celsius = averageCelsius;
+    lcd.SetCurrentHeatingTemperature(celsius);
+    averageCelsiusMillis = currentMillis;
+    averageCelsiusCount = 1;
+  }
 }
 
 void loop() {
   wdt_reset();
-  printTime();  
-  unsigned long currentMillis = millis();
-  computeOutsideTemperature(currentMillis);
+  currentMillis = millis();
+  outsideTemperatureSensor.CheckTemperature();
+  lcd.Print();
   if(currentMillis - lastReadCelsius > 1000)
   {
     readCurrentHeatingTemperature();
@@ -190,13 +213,15 @@ void loop() {
   }
   if(!relayOn)
   {
-    checkHeating(currentMillis);    
+    checkHeating();    
   }
   if(relayOn && currentMillis - relayOnMillis > interval)
   {
     setRelayOff();
     position += interval * direction;
     position = min(max(position, 0), 120000);
+    //Serial.print(F("Position: "));
+    //Serial.println(position);
     relayOffMillis = currentMillis;
   }
   if(!heatingOff && !relayOn && currentMillis - relayOffMillis > 45000)
@@ -207,5 +232,9 @@ void loop() {
       setRelay(value, celsius);
       relayOnMillis = currentMillis;
     }
+  }
+  if(currentMillis - lastReadOutTemperature > 1000*60*5)
+  {
+    //Serial.println("Error read outside temperature");
   }
 }
