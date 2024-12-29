7371b3a36abdaba8ed59938ad8f063146c2f2902
Zefek <32429493+Zefek@users.noreply.github.com>
2024-12-29 19:17:45 +0100

    Ekviterm (#3)
    * Squashed commit of the following:
    
    commit 41c42dfe1ad6220589fa8e0f557d9ba1ceb1d7cf
    Author: Zefek <32429493+Zefek@users.noreply.github.com>
    Date:   Sat Dec 14 18:35:33 2024 +0100
    
        Home assistant modes (#2)
    
    commit 4f56cebc85fd88274e3d207a41ae315042792114
    Merge: cf81f0c 32833fb
    Author: Zefek <32429493+Zefek@users.noreply.github.com>
    Date:   Thu Dec 5 12:24:30 2024 +0100
    
        Merge pull request #1 from Zefek/HomeAssistant
    
        Home assistant
    
    commit 32833fb6bb2844bb376939cf33a4442e918908b2
    Author: Zefek <petr.sponky@gmail.com>
    Date:   Thu Dec 5 12:21:25 2024 +0100
    
        Ladění
    
    commit ff6babc1bcab499da498189f33e448dc59fb8a4d
    Author: Zefek <petr.sponky@gmail.com>
    Date:   Wed Dec 4 16:51:52 2024 +0100
    
        Doladění
    
    commit 4d7520d50d1f3ff163ccbfa710b49f5c0fbfe011
    Author: Zefek <petr.sponky@gmail.com>
    Date:   Tue Oct 8 19:37:01 2024 +0200
    
        Home assistant
    
    commit c048e545ccf45fb3451c6e2f07697d7a2cd842c8
    Author: Zefek <petr.sponky@gmail.com>
    Date:   Tue Oct 8 19:33:46 2024 +0200
    
        Squashed commit of the following:
    
        commit 28c7d7926610eff24df2a7e9d8be8503b72be2f6
        Author: Zefek <petr.sponky@gmail.com>
        Date:   Tue Oct 8 19:33:04 2024 +0200
    
            Homeassistant
    
    * Nastavení topné křivky
    
    * Úpravy pro homeassistant

--- rizeni_teploty_kotel.ino
+++ rizeni_teploty_kotel.ino
@@ -5,6 +5,7 @@
 #include "TemperatureSensor.h"
 #include "HomeAssistant.h"
 #include "config.h"
+#include <avr/wdt.h>
 
 #define I2C_ADDR            0x27
 #define LCD_COLUMNS         16
@@ -25,18 +26,18 @@ void OutsideTemperatureChanged(double temperature, int channel, int humidity, in
 void MQTTMessageReceive(char* topic, uint8_t* payload, unsigned int length);
 void TimeChanged(int hours, int minutes);
 void computeRequiredTemperature();
+void OnMQTTConnected(bool success);
 
 
 Display lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 Ds1302 rtc(4, 5, 6);
 TemperatureSensor outsideTemperatureSensor(2, OutsideTemperatureChanged);
-HomeAssistant homeAssistant(WifiSSID, WifiPassword, MQTTUsername, MQTTPassword, MQTTHost, "Heating", MQTTMessageReceive);
+HomeAssistant homeAssistant(WifiSSID, WifiPassword, MQTTUsername, MQTTPassword, MQTTHost, "Heating", MQTTMessageReceive, OnMQTTConnected);
 unsigned long relayOnMillis = 0;
 unsigned long relayOffMillis = 0;
 unsigned long currentMillis = 0;
 unsigned long temperatureReadMillis = 0;
 unsigned long lastMQTTSendMillis = 0;
-unsigned long lastMQTTConnectTry = 0;
 long interval = 0;
 long position = 70000;
 bool relayOn = false;
@@ -48,8 +49,6 @@ short direction = 0;
 bool heatingOff = true;
 double inputTemperature = 25;
 double outsideTemperature = 14;
-double currentInsideTemperature = 22;
-double requiredInsideTemperature = 22;
 int sensor = 1;
 bool manualValve = false;
 bool autoHeating = true;
@@ -57,20 +56,24 @@ bool thermostat = false;
 double totalPower = 0;
 int sensrId = 0;
 int mode = 1; //0 - Off, 1 - Automat, 2 - Thermostat
-int setTemperatureMode = 0; //0 - Automat, 1 - Manual
+int equithermalCurveZeroPoint = 41;
+double insideTemperature = 23;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial1.begin(115200);
   byte msb = EEPROM.read(0);
   byte lsb = EEPROM.read(1);
   sensrId = (msb << 4) + lsb;
   Serial.print("Sensor Id: ");
   Serial.println(sensrId);
-  homeAssistant.Init();
   rtc.init();
   lcd.Init(&rtc, TimeChanged);
   lcd.BackLight();
+  outsideTemperatureSensor.Init();
+  homeAssistant.Init();
+  homeAssistant.Connect();
+  delay(1000);
   pinMode(MOREHEATINGRELAYPIN, OUTPUT);
   pinMode(LESSHEATINGRELAYPIN, OUTPUT);
   pinMode(HEATINGPUMPRELAYPIN, OUTPUT);
@@ -78,16 +81,30 @@ void setup() {
   digitalWrite(MOREHEATINGRELAYPIN, HIGH);
   digitalWrite(LESSHEATINGRELAYPIN, HIGH);
   digitalWrite(HEATINGPUMPRELAYPIN, HIGH);
-  int s = homeAssistant.Connect();
-  homeAssistant.Subscribe("homeassistant/devices/heater/command/*");
-  homeAssistant.Subscribe("homeassistant/status");
-  homeAssistant.Subscribe("homeassistant/devices/heater/events/*");
-  delay(1000);
-  outsideTemperatureSensor.Init();
-  lcd.SetCurrentHeatingTemperature(celsius);
-  lcd.SetInputTemperature(inputTemperature);
-  lcd.SetOutTemperature(outsideTemperature);
-  computeRequiredTemperature();
+  lcd.SetMode(mode);
+  readCurrentHeatingTemperature();
+  readCurrentReturnTemperature();
+  readInputTemperature();
+  computeEnergy();
+  lcd.Print();
+  wdt_enable(WDTO_8S);
+}
+
+void OnMQTTConnected(bool success)
+{
+  if(success)
+  {
+    homeAssistant.Subscribe("homeassistant/devices/heater/command/*");
+    homeAssistant.Subscribe("homeassistant/status");
+    homeAssistant.Subscribe("homeassistant/devices/heater/events/*");
+  }
+  if(!success)
+  {
+    mode = 1;
+    lcd.SetMode(mode);
+    equithermalCurveZeroPoint = 41;
+    insideTemperature = 23;
+  }
 }
 
 void TimeChanged(int hours, int minutes)
@@ -106,8 +123,14 @@ void MQTTMessageReceive(char* topic, uint8_t* payload, unsigned int length)
   //Termostat on/off
   if(strcmp(topic, "homeassistant/devices/heater/command/thermostat") == 0)
   {
-    thermostat = !thermostat;
-    homeAssistant.SetSensor(thermostat?"ON":"OFF", "homeassistant/devices/heater/thermostat");
+    if(strcmp(p, "OFF") == 0)
+    {
+       thermostat = false;
+    }
+    else if(strcmp(p, "ON") == 0)
+    {
+      thermostat = true;
+    }
   }
   //Nastavení módu - Off (vypnuto), Automatic (automatické), Thermostat (ovládání termostatem)
   if(strcmp(topic, "homeassistant/devices/heater/command/mode") == 0)
@@ -124,32 +147,14 @@ void MQTTMessageReceive(char* topic, uint8_t* payload, unsigned int length)
     {
       mode = 2;
     }
-    homeAssistant.SetSensor(p, "homeassistant/devices/heater/mode");
     lcd.SetMode(mode);
   }
-  //Mód topné křivky - Automatický, manuální
-  if(strcmp(topic, "homeassistant/devices/heater/command/settemperaturemode") == 0)
-  {
-    if(strcmp(p, "Automatic") == 0)
-    { 
-      setTemperatureMode = 0;
-    }
-    if(strcmp(p, "Manual") == 0)
-    {
-        setTemperatureMode = 1;
-    }
-    homeAssistant.SetSensor(p, "homeassistant/devices/heater/settemperaturemode");
-  }
-  //Nastavená teplota topné vody
-  if(strcmp(topic, "homeassistant/devices/heater/command/settemperature") == 0)
+  //Bod na nule v topné křivce
+  if(strcmp(topic, "homeassistant/devices/heater/command/zeroPoint") == 0)
   {
     int t = 0;
     sscanf(p, "%d", &t);
-    value = t;
-    setTemperatureMode = 1;
-    lcd.SetRequiredTemperature(value);
-    homeAssistant.SetSensor(value, "homeassistant/devices/heater/setTemperature");
-    homeAssistant.SetSensor("Manual", "homeassistant/devices/heater/settemperaturemode");
+    equithermalCurveZeroPoint = t;
   }
   //Nastavení data a času
   if(strcmp(topic, "homeassistant/devices/heater/command/currentDateTime") == 0)
@@ -164,6 +169,10 @@ void MQTTMessageReceive(char* topic, uint8_t* payload, unsigned int length)
     dt.month = month;
     dt.year = year;
     rtc.setDateTime(&dt);
+  }
+  if(strcmp(topic, "homeassistant/devices/heater/events/insidetemperaturesetchanged") == 0)
+  {
+    insideTemperature = atof(p);
   }
 }
 
@@ -186,23 +195,17 @@ void OutsideTemperatureChanged(double temperature, int channel, int humidity, in
   
   if(channel == 1 && sensrId == sensorId)
   {
-    //if(outsideTemperature != temperature)
-    {
-      outsideTemperature = temperature;
-      JsonDocument doc;
-      doc["t"] = temperature;
-      doc["h"] = humidity;
-      doc["b"] = batteryLow;
-      doc["tt"] = temperatureTrend;
-      String output;
-      serializeJson(doc, output);
-      lcd.SetOutTemperature(temperature);
-
-      homeAssistant.SetSensor(output.c_str(), "homeassistant/devices/heater/outsideTemperature");
-      Serial.println(output);
-      lcd.SetOutTemperature(temperature);
-      computeRequiredTemperature();
-    }
+    outsideTemperature = temperature;
+    JsonDocument doc;
+    doc["t"] = temperature;
+    doc["h"] = humidity;
+    doc["b"] = batteryLow;
+    doc["tt"] = temperatureTrend;
+    String output;
+    serializeJson(doc, output);
+    homeAssistant.SetSensor(output.c_str(), "homeassistant/devices/heater/outsideTemperature", true);
+    lcd.SetOutTemperature(temperature);
+    computeRequiredTemperature();
   }
 }
 
@@ -213,23 +216,16 @@ double getTemperature(double pinValue, int beta)
 
 void computeRequiredTemperature()
 {
-  if(setTemperatureMode == 1)
-  {
-    return;
-  }
-  //nastavená teplota v místnosti
-  double inTemp = 22.5;
   //nastavená teplota topné vody pro venkovní teplotu 0°C
   //touto proměnnou se nastavuje sklon topné křivky.
-  double zeroTemp = 40;
+  double zeroTemp = equithermalCurveZeroPoint;
   Ds1302::DateTime now;
   rtc.getDateTime(&now);
   if(now.hour < 15 || now.hour >= 23)
   {
-    inTemp = 21.5;
-    zeroTemp = 38;
+    zeroTemp = equithermalCurveZeroPoint - 4;
   }
-  int newValue = (int)round(inTemp + (zeroTemp - inTemp) * pow((outsideTemperature - inTemp) / (double) - inTemp, 0.76923));
+  int newValue = (int)round(insideTemperature + (zeroTemp - insideTemperature) * pow((outsideTemperature - insideTemperature) / (double) - insideTemperature, 0.76923));
 
   if(newValue < 10 || newValue > 80)
   {
@@ -239,7 +235,7 @@ void computeRequiredTemperature()
   {
     value = newValue;
     lcd.SetRequiredTemperature(value);
-    homeAssistant.SetSensor(value, "homeassistant/devices/heater/setTemperature");
+    homeAssistant.SetSensor(value, "homeassistant/devices/heater/setTemperature", true);
   }
 }
 
@@ -321,13 +317,13 @@ void checkHeating()
     relayOn = true;
     relayOnMillis = currentMillis;
     homeAssistant.SetSensor("ON", "homeassistant/devices/heater/valvemovement");
-    homeAssistant.SetSensor("OFF", "homeassistant/devices/heater/heater");
+    homeAssistant.SetSensor("OFF", "homeassistant/devices/heater/heater", true);
   }
   if(heatingOff && ShouldBeHeatingOn())
   {
     heatingOff = false;
     digitalWrite(HEATINGPUMPRELAYPIN, LOW);
-    homeAssistant.SetSensor("ON", "homeassistant/devices/heater/heater");
+    homeAssistant.SetSensor("ON", "homeassistant/devices/heater/heater", true);
   }
   lcd.SetHeating(!heatingOff);
 }
@@ -341,7 +337,7 @@ void readInputTemperature()
   }
   if((int)round(inputTemperature) != (int)round(newInputTemperature))
   { 
-    lcd.SetInputTemperature((int)round(inputTemperature));
+    lcd.SetInputTemperature((int)round(newInputTemperature));
   }
   if(newInputTemperature < inputTemperature - 0.5 || newInputTemperature > inputTemperature + 0.5)
   {
@@ -402,13 +398,11 @@ void sendToHomeAssistant()
 }
 
 void loop() {
+  wdt_reset();
   currentMillis = millis();
   if(!homeAssistant.Loop())
   {
     homeAssistant.Connect();
-    homeAssistant.Subscribe("homeassistant/devices/heater/command/*");
-    homeAssistant.Subscribe("homeassistant/status");
-    homeAssistant.Subscribe("homeassistant/devices/heater/events/*");
   }
   
   outsideTemperatureSensor.CheckTemperature();
